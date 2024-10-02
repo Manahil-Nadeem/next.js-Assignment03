fbdfcca0b0b0660616d58ef6ad3104959965a5ad
Manahil-Nadeem <manahilnadeem068@gmail.com>
2024-10-03 00:51:15 +0500

    Add files via upload

---
+++ page.tsx
@@ -0,0 +1,15 @@
+import Image from "next/image";
+import profile from"../../pubic/profile.jpg";
+
+
+import  Header from  "@/app/components/header";
+
+export default function Home(){
+  return (
+    <div>
+   
+     
+    </div>
+  )
+}
+
